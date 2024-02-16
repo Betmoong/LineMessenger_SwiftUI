2fec1452a2a01716c293765ac88928cebaf3759f
Rocky <rdev99@naver.com>
2024-02-16 10:18:58 +0900

    Create Project

---
+++ LineMessenger/LineMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineMessengerApp.swift
+//  LineMessenger
+//
+//  Created by Rocky on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct LineMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
