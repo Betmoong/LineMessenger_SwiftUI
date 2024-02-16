6932036cafdf86271833200617cdeb473fbef412
Rocky <rdev99@naver.com>
2024-02-16 14:10:54 +0900

    feat: LoginView UI 작업

--- LineMessenger/View/Login/LoginView.swift
+++ LineMessenger/View/Login/LoginView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("LoginView")
+        VStack(alignment: .leading) {
+            Group {
+                Text("로그인")
+                    .font(.system(size: 28, weight: .bold))
+                    .foregroundStyle(.bkText)
+                    .padding(.top, 80)
+                Text("아래 제공되는 서비스로 로그인을 해주세요.")
+                    .font(.system(size: 14))
+                    .foregroundStyle(.greyDeep)
+            }
+            .padding(.horizontal, 30)
+            
+            Spacer()
+            
+            Button {
+                // TODO: - 구글 로그인
+            } label: {
+                Text("Google로 로그인")
+            }.buttonStyle(LoginButtonStyle(textColor: .bkText, borderColor: .greyLight))
+            
+            Button {
+                // TODO: - 애플 로그인
+            } label: {
+                Text("Apple로 로그인")
+            }.buttonStyle(LoginButtonStyle(textColor: .bkText, borderColor: .greyLight))
+        }
     }
 }
 
