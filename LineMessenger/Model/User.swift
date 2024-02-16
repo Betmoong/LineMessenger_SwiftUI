2f61ff96ca49015eaba05d6e860db135e1b31b7a
Rocky <rdev99@naver.com>
2024-02-16 16:04:01 +0900

    feat: 구글 로그인 연동

---
+++ LineMessenger/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  LineMessenger
+//
+//  Created by Rocky on 2/16/24.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var name: String
+    var phoneNumber: String?
+    var profileURL: String?
+    var description: String?
+}
---
+++ LineMessenger/Service/ServiceError.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceError.swift
+//  LineMessenger
+//
+//  Created by Rocky on 2/16/24.
+//
+
+import Foundation
+
+enum ServiceError: Error {
+    case error(Error)
+}
